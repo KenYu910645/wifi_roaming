8d9d45c7f5054e70ae9f1bc2f000b4cf5e43f7d2
spiderkiller13 <spiderkiller6@gmail.com>
2018-10-07 23:36:37 +0800

    dev

--- wifi_roaming.sh
+++ wifi_roaming.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-current_wifi=$(nmcli -f in-use,ssid,bssid,signal,bars dev wifi | grep  \* | tail -n1) # | awk '{ print $2 }'
+# current_wifi=$(nmcli -f in-use,ssid,bssid,signal,bars dev wifi | grep  \* | tail -n1) # | awk '{ print $2 }'
+nmcli -f in-use,ssid,bssid,signal dev wifi | grep -n \* -B999 | awk '{print $1 " " $2 " " $3 "  " $4}' > temp.txt
 
-current_SSID=$(echo ${current_wifi:1} | awk '{print $1}')
-current_BSSID=$(echo ${current_wifi:1} | awk '{print $2}')
-current_SIGNAL=$(echo ${current_wifi:1} | awk '{print $3}')
+
+current_SSID=$(  cat temp.txt | grep \* | tail -n1 | awk '{print $2}') 
+current_BSSID=$( cat temp.txt | grep \* | tail -n1 | awk '{print $3}') 
+current_SIGNAL=$(cat temp.txt | grep \* | tail -n1 | awk '{print $4}') 
+
+
 
 
 echo "current_wifi : ${current_wifi}" 
